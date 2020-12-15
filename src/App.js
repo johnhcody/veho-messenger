ac49d21dbcc469869491545f3148cb728ac9d2e1
johnhcody <john.haner.cody@gmail.com>
2020-12-15 13:48:32 +0700

    main route complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import MainPage from './components/main/main';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload test again.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={MainPage}></Route>
+          
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/main/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+//import '../../stylesheets/application';
+
+class MainPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>hello from main page</p>
+            </div>
+        )
+    }
+}
+
+export default MainPage;
