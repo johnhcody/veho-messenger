cc8984723a3f485b15602b5339ed51a32bb3adac
johnhcody <john.haner.cody@gmail.com>
2020-12-15 20:35:21 +0700

    pushing with new README

--- src/root.js
+++ src/root.js
@@ -4,11 +4,9 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const Root = () => (
-
         <HashRouter>
             <App />
         </HashRouter>
-
 );
 
 export default Root;
