b831764b9b176a3706e88f1825f33dd9352eebff
johnhcody <john.haner.cody@gmail.com>
2020-12-15 20:27:24 +0700

    ready to submit

--- src/components/messages/chat_index.jsx
+++ src/components/messages/chat_index.jsx
@@ -14,10 +14,18 @@ class ChatIndex extends React.Component {
     }
 
 
-    // componentDidMount() {
+    componentDidMount() {
+        setTimeout(
+            () => this.setState(prevState => ({ ready: !prevState.ready })),
+            Math.floor(Math.random() * 50000),
+        );
+        // setTimeout(function () { this.setState({ ready: true }); }, Math.floor(Math.random() * 5000));
+        // setTimeOut(() => this.setState({ ready: true }), Math.floor(Math.random() * 50000))
+    }
 
-    //     this.setTimeOut(() => this.setState({ ready: true }), Math.floor(Math.random() * 50000))
-    // }
+    componentWillUnmount() {
+        //clearInterval(this.timer);
+    }
 
     render() {
 
@@ -56,6 +64,39 @@ class ChatIndex extends React.Component {
                 </>
             )
 
+        } else if (this.state.ready) {
+            let userInfo = window.localStorage.getItem([userId]).split(',');
+            let firstName = userInfo[0];
+            let lastName = userInfo[1];
+            let body = userInfo[2];
+            let timeStamp = userInfo[userInfo.length - 1];
+            let response = window.localStorage.getItem('response');
+            debugger
+            return (
+                <>
+                    <div className="chat-box-wrapper">
+                        <div className="chat-text-wrapper">
+                            <p>{body}</p>
+                        </div>
+                    </div>
+                    <div className="chat-icon-wrapper">
+                        {/* G U for Guest User */}
+                        <button>GU</button>
+                        <p>{timeStamp}</p>
+                    </div>
+                    <div className="response-wrapper">
+                        <div className="chat-box-response">
+                            <div className="chat-text-wrapper">
+                                <p>{response}</p>
+                            </div>
+                        </div>
+                        <div className="chat-icon-wrapper-response">
+                            <button>{`${firstName[0]}${lastName[0]}`}</button>
+                            <p>{timeStamp}</p>
+                        </div>
+                    </div>
+                </>
+            )
         }
         // }
     }
